d65964f6e4257cecba8d63ae7836f7740b14d880
Mathijs <mathijspb@gmail.com>
2022-03-22 14:48:03 +0100

    :wrench: Config > Disable google analytics plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -3,28 +3,39 @@ module.exports = {
     title: `new`,
     siteUrl: `https://www.yourdomain.tld`
   },
-  plugins: [{
-    resolve: 'gatsby-source-contentful',
-    options: {
-      "accessToken": "dv-84QhmtpDlI1CGNT18z5zo5bqbCwOR_cjfX7yTX6g",
-      "spaceId": "y9dmurrpfk8p"
-    }
-  }, "gatsby-plugin-sass", {
-    resolve: 'gatsby-plugin-google-analytics',
-    options: {
-      "trackingId": ""
-    }
-  }, "gatsby-plugin-image", "gatsby-plugin-react-helmet", "gatsby-plugin-sitemap", {
-    resolve: 'gatsby-plugin-manifest',
-    options: {
-      "icon": "src/images/icon.png"
-    }
-  }, "gatsby-plugin-sharp", "gatsby-transformer-sharp", {
-    resolve: 'gatsby-source-filesystem',
-    options: {
-      "name": "images",
-      "path": "./src/images/"
+  plugins: [
+    {
+      resolve: 'gatsby-source-contentful',
+      options: {
+        "accessToken": "dv-84QhmtpDlI1CGNT18z5zo5bqbCwOR_cjfX7yTX6g",
+        "spaceId": "y9dmurrpfk8p"
+      }
     },
-    __key: "images"
-  }]
+    "gatsby-plugin-sass",
+    // {
+    //   resolve: 'gatsby-plugin-google-analytics',
+    //   options: {
+    //     "trackingId": ""
+    //   }
+    // },
+    "gatsby-plugin-image",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sitemap",
+    {
+      resolve: 'gatsby-plugin-manifest',
+      options: {
+        "icon": "src/images/icon.png"
+      }
+    },
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        "name": "images",
+        "path": "./src/images/"
+      },
+      __key: "images"
+    }
+  ]
 };
