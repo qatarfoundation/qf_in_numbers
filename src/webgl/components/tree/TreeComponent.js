95f09ee4f2d4ae45f167019018859cede5a79cae
Mathijs <mathijspb@gmail.com>
2022-04-01 20:06:01 +0200

    :construction: Webgl > Tree > Improve branches

--- src/webgl/components/tree/TreeComponent.js
+++ src/webgl/components/tree/TreeComponent.js
@@ -13,7 +13,7 @@ export default class TreeComponent extends component(Object3D) {
      */
     _createMesh() {
         const gltf = ResourceLoader.get('tree');
-        const model = gltf.scene.getObjectByName('Plane');
+        const model = gltf.scene.getObjectByName('Plane').clone();
         this.add(model);
 
         const positions = model.geometry.getAttribute('position').array;
@@ -23,31 +23,117 @@ export default class TreeComponent extends component(Object3D) {
             pointss.push(new Vector3(positions[i + 0], positions[i + 1], positions[i + 2]));
         }
 
-        const curve = new CatmullRomCurve3(pointss);
+        const branches = this._parseBranches(model);
 
         const amount = 1000;
 
         const geometry = new SphereGeometry(0.1);
         const material = new MeshBasicMaterial();
 
-        const mesh = new InstancedMesh(geometry, material, amount);
+        const mesh = new InstancedMesh(geometry, material, amount * branches.length);
         this.add(mesh);
 
         const dummy = new Object3D();
 
-        for (let i = 0; i < amount; i++) {
-            const point = curve.getPointAt(Math.random());
+        let index = 0;
+        for (let i = 0, len = branches.length; i < len; i++) {
+            const curve = new CatmullRomCurve3(branches[i]);
 
-            const radius = 1.0 - point.y / 20;
+            for (let i = 0; i < amount; i++) {
+                const point = curve.getPointAt(Math.random());
 
-            const angle = Math.random() * Math.PI * 2;
-            const x = point.x + radius * Math.cos(angle);
-            const z = point.z + radius * Math.sin(angle);
+                const radius = (1.0 - point.y / 30) * 1;
+                // const radius = 0.2;
 
-            dummy.position.set(x, point.y, z);
-            dummy.updateMatrix();
-            mesh.setMatrixAt(i, dummy.matrix);
+                const angle = Math.random() * Math.PI * 2;
+                const x = point.x + radius * Math.cos(angle);
+                const z = point.z + radius * Math.sin(angle);
+
+                dummy.position.set(x, point.y, z);
+                dummy.updateMatrix();
+                mesh.setMatrixAt(index, dummy.matrix);
+
+                index++;
+            }
         }
+    }
+
+    _parseBranches(model) {
+        const positions = model.geometry.getAttribute('position').array;
+        const indices = model.geometry.index.array;
+
+        const vertices = [];
+        for (let i = 0, len = positions.length; i < len; i += 3) {
+            vertices.push(new Vector3(positions[i + 0], positions[i + 1], positions[i + 2]));
+        }
+
+        const indicesBranches = [[]];
+        let current = indicesBranches[0];
+        let previousEnd = 0;
+
+        for (let i = 0, len = indices.length; i < len; i += 2) {
+            const start = indices[i + 0];
+            const end = indices[i + 1];
+
+            if (start !== previousEnd) {
+                const isInBranches = this._isInBranches(indicesBranches, start);
+                if (isInBranches) {
+                    isInBranches.push(start, end);
+                } else {
+                    current = [];
+                    indicesBranches.push(current);
+                    current.push(start, end);
+                }
+            } else if (this._isInBranch(current, start + 1)) {
+                current = [];
+                indicesBranches.push(current);
+                current.push(start, end);
+            } else {
+                current.push(start, end);
+            }
+
+            previousEnd = end;
+        }
+
+        const branches = [];
+        let newIndex = 0;
+        for (let i = 0, len = indicesBranches.length; i < len; i++) {
+            const branch = indicesBranches[i];
+
+            if (branch.length > 0) {
+                branches[newIndex] = [];
+                for (let j = 0, lenJ = branch.length; j < lenJ; j++) {
+                    const index = branch[j];
+                    const x = positions[index * 3 + 0];
+                    const y = positions[index * 3 + 1];
+                    const z = positions[index * 3 + 2];
+                    branches[newIndex].push(new Vector3(x, y, z));
+                }
+                newIndex++;
+            }
+        }
+
+        return branches;
+        // return [branches[62]];
+    }
+
+    _isInBranch(branch, index)  {
+        for (let i = 0, len = branch.length; i < len; i += 2) {
+            const end = branch[i + 1];
+            if (end === index) return true;
+        }
+        return false;
+    }
+
+    _isInBranches(branches, index)  {
+        for (let i = 0, len = branches.length; i < len; i++) {
+            const branch = branches[i];
+            for (let j = 0, lenJ = branch.length; j < lenJ; j += 2) {
+                const end = branch[j + 1];
+                if (end === index && j + 1 === lenJ - 1) return branch;
+            }
+        }
+        return false;
     }
 
     /**
