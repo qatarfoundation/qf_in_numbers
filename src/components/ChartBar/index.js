09986a8fb56f5c5d22a06b879fd3c68e3407c1ee
rmdms <remy.benjamin.dumas@gmail.com>
2022-05-11 15:40:42 +0900

    :sparkles: Bar chart > Update : remap value by width container

--- src/components/ChartBar/index.js
+++ src/components/ChartBar/index.js
@@ -20,38 +20,43 @@ function ChartBar(props, ref) {
         return Math.floor(Math.random() * (max - min) + min);
     }
     const data = [
-        { title: 'Gold', value: 2, additionalInformation: Math.random() > 0.5 ? `${ getRandomArbitrary(0, 100) }% of disclosures` : ''  },
-        { title: 'Silver', value: 3, additionalInformation: Math.random() > 0.5 ? `${ getRandomArbitrary(0, 100) }% of disclosures` : '' },
-        { title: 'Bronze', value: 1, additionalInformation: Math.random() > 0.5 ? `${ getRandomArbitrary(0, 100) }% of disclosures` : '' },
+        { title: 'Gold', value: getRandomArbitrary(0, 1000000), additionalInformation: Math.random() > 0.5 ? `${ getRandomArbitrary(0, 100) }% of disclosures` : ''  },
+        { title: 'Silver', value: getRandomArbitrary(0, 1000000), additionalInformation: Math.random() > 0.5 ? `${ getRandomArbitrary(0, 100) }% of disclosures` : '' },
+        { title: 'Bronze', value: getRandomArbitrary(0, 1000000), additionalInformation: Math.random() > 0.5 ? `${ getRandomArbitrary(0, 100) }% of disclosures` : '' },
     ];
     const xValue = d => d.value;
     const yValue = d => d.title;
     const xSubvalue = d => d.additionalInformation;
+    const heightBar = 16;
+    const paddingBar = 27;
+    const lengthY = data.map(yValue).filter((item, pos) => data.map(yValue).indexOf(item) == pos).length;
+    // const margin = { top: 0, right: window.innerWidth >= 500 ? 67 : 23, bottom: 0, left: window.innerWidth >= 500 ? window.innerWidth >= 1440 ? 353 : window.innerWidth * 353 / 1440 : window.innerWidth * 200 / 499 };
     /**
      * States
      */
-    const [width, setWidth] = useState(window.innerWidth >= 500 ? 1140 : 590);
-    const [height, setHeight] = useState(data.map(yValue).length * (16 + 27));
+    const [margin, setMatgin] = useState({ top: 0, right: window.innerWidth >= 500 ? 67 : 23, bottom: 0, left: window.innerWidth >= 500 ? window.innerWidth >= 1440 ? 353 : window.innerWidth * 353 / 1440 : window.innerWidth * 200 / 499 });
+    const [height, setHeight] = useState((heightBar + paddingBar) * lengthY + margin.top + margin.bottom);
+    console.log('d3', height);
     /**
     * References
     */
     const refChart = useD3(
         (svg) => {
             svg.select('.chart-container').remove();
-            const w = width;
+            const w = refChart.current.clientWidth;
             const h = height;
-            const margin = { top: 0, right: 50, bottom: 0, left: window.innerWidth >= 500 ? window.innerWidth * 335 / 1440 : window.innerWidth * 121 / 499 };
             const innerWidth = w - margin.left - margin.right;
             const innerHeight = h - margin.top - margin.bottom;
+            console.log('d3', margin.top, margin.bottom);
             const xScale = d3
                 .scaleLinear()
-                .domain([0, 150 * d3.max(data, xValue) / 100])
-                .range([0, innerWidth]);
+                .domain([0, d3.max(data, xValue)])
+                .range([0, innerWidth - 120]);
             const yScale = d3
                 .scaleBand()
                 .domain(data.map(yValue))
                 .range([0, innerHeight])
-                .padding(0.55);
+                .padding(0.2);
             const chartContainer = svg
                 .append('g')
                 .attr('class', 'chart-container')
@@ -63,7 +68,7 @@ function ChartBar(props, ref) {
                 .call(d3.axisLeft(yScale).tickSize(0));
             const ticks = d3
                 .selectAll('.tick text')
-                .attr('class', 'p7');
+                .attr('class', 'p4');
             const barsContainer = chartContainer
                 .append('g')
                 .attr('class', 'bars-container');
@@ -79,23 +84,23 @@ function ChartBar(props, ref) {
                 .append('rect')
                 .attr('class', 'bar')
                 .attr('width', d => xScale(xValue(d)))
-                .attr('height', yScale.bandwidth());
+                .attr('height', heightBar);
             barContainer
                 .append('text')
                 .attr('class', 'p2 label')
                 .text(d => xValue(d))
                 .attr('x', d => xScale(xValue(d)) + 11)
                 .attr('y', d => yScale.bandwidth() / 2)
-                .attr('dy', '.35em');
+                .attr('dy', '0');
             barContainer
                 .append('text')
                 .attr('class', 'p6 sublabel')
                 .text(d => xSubvalue(d))
                 .attr('x', d => xScale(xValue(d)) + 11)
                 .attr('y', d => yScale.bandwidth())
-                .attr('dy', '1em');
+                .attr('dy', '0');
         },
-        [data.length, width],
+        [data.length, margin],
     );
     /**
      * Events
@@ -111,25 +116,17 @@ function ChartBar(props, ref) {
      * Private
      */
     function resize() {
-        if (window.innerWidth >= 500) {
-            if (width !== 1140) {
-                setWidth(1140);
-            }
-            if (width !== 590) {
-                setWidth(590);
-            }
-        }
+        setMatgin({ top: 0, right: window.innerWidth >= 500 ? 67 : 23, bottom: 0, left: window.innerWidth >= 500 ? window.innerWidth >= 1440 ? 353 : window.innerWidth * 353 / 1440 : window.innerWidth * 200 / 499 });
+        // setWidth('100%');
+        // setWidth(refChart.current.clientWidth);
     }
     return (
-        <>
-            <svg
-                ref={ refChart }
-                width={ width }
-                height={ height }
-                // viewBox={ `0 0 ${ height } ${ width }` }
-                className="chart chart-bar"
-            />
-        </>
+        <svg
+            ref={ refChart }
+            height={ height }
+            // viewBox={ `0 0 ${ height } ${ width }` }
+            className="chart chart-bar"
+        />
     );
 }
 
