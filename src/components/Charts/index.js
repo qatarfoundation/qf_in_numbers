63e2c56e1b9e3a24dc1deb9829e099b1231cdbee
rmdms <remy.benjamin.dumas@gmail.com>
2022-05-27 19:56:34 +0900

    :bug: Charts > Update multiple charts

--- src/components/Charts/index.js
+++ src/components/Charts/index.js
@@ -19,10 +19,98 @@ function Charts(props, ref) {
     /**
      * Datas
      */
-    const { charts } = props;
+    let { charts } = props;
+    charts = Object.values(
+        charts.reduce(
+            (a, v) => ((a[v.type] || (a[v.type] = [])).push(v), a), {},
+        ),
+    ).map(c => {
+        const result = {};
+        result[c[0].type] = c;
+        return result;
+    });
     return (
         <>
             {
+                charts.map((chart, i) => {
+                    const result = [];
+                    chart[Object.keys(chart)[0]].map((c, j) => {
+                        const r = {};
+                        if (c.title) {
+                            r.title = (<><h2 className="p2 section-container">{ c.title.map(t => {
+                                let el = undefined;
+                                if (t.bold) {
+                                    el = <span key={ t.value } className='bold'>{ t.value }</span>;
+                                } else {
+                                    el = t.value;
+                                }
+                                return el;
+                            }) }</h2></>);
+                        }
+                        if (c.subtitle) {
+                            r.subtitle = (<><p className="p6 subtitle section-container">{ c.subtitle }</p></>);
+                        }
+                        if (c.fields) {
+                            switch (c.type) {
+                                case 'kpiChart':
+                                    r.chart = (<><ChartKPI chart={ c } /></>);
+                                    break;
+                                case 'heatmapChart':
+                                    r.chart = (<><ChartHeatmap chart={ c } /></>);
+                                    break;
+                                case 'barChart':
+                                    r.chart = (<><ChartBar chart={ c } /></>);
+                                    break;
+                                case 'donutChart':
+                                    r.chart = (<><ChartDonut chart={ c } /></>);
+                                    break;
+                                case 'lineChart':
+                                    r.chart = (<><ChartLine chart={ c } /></>);
+                                    break;
+                                case 'bubbleChart':
+                                    r.chart = (<><ChartBubble chart={ c } /></>);
+                                    break;
+                                case 'mapChart':
+                                    r.chart = (<><ChartMap chart={ c } /></>);
+                                    break;
+                                case 'beeswarmChart':
+                                    r.chart = (<><ChartBeeswarm chart={ c } /></>);
+                                    break;
+                            }
+                        }
+                        result.push(r);
+                    });
+                    console.log(result);
+                    return <section key={ i } className="section charts" data-name={ chart.type }>
+                        { result.length > 1 ?
+                            <Scrollbar colored={ false }>
+                                <div className='charts-container'>
+                                    { result.map((r, i) =>
+                                        <div className='charts-item' key={ i }>
+                                            { r.title && r.title }
+                                            { r.subtitle && r.subtitle }
+                                            { r.chart && r.chart }
+                                        </div>,
+                                    ) }
+                                </div>
+                            </Scrollbar>
+                            :
+                            <>{ result.map((r, i) =>
+                                <div className='charts-item' key={ i }>
+                                    { r.title && r.title }
+                                    { r.subtitle && r.subtitle }
+                                    <Scrollbar colored={ false }>
+                                        <div className='charts-container'>
+                                            { r.chart && r.chart }
+                                        </div>
+                                    </Scrollbar>
+                                </div>,
+                            ) }</>
+                        }
+                    </section>;
+                })
+            }
+            { /* {
                 charts.map((chart, i) => {
                     let result = <></>;
                     if (chart.fields) {
@@ -69,7 +157,7 @@ function Charts(props, ref) {
                         </Scrollbar>
                     </section>;
                 })
-            }
+            } */ }
         </>
     );
 }
