76ef716abef2a02a72c4713cb3c929e56ef8ed4b
Davy <davy@60fps.fr>
2022-07-22 15:21:27 +0200

    entity particles movements

--- src/webgl/shaders/entity-particles/vertex.glsl
+++ src/webgl/shaders/entity-particles/vertex.glsl
@@ -1,6 +1,7 @@
 // Attributes
 attribute float size;
 attribute float color;
+attribute vec2 displacement;
 
 // Varyings
 varying vec2 vUv;
@@ -8,6 +9,7 @@ varying float vColor;
 
 // Uniforms
 uniform float uPointSize;
+uniform float uTime;
 
 void main() {
     // Transformed
@@ -15,7 +17,10 @@ void main() {
 
     // Output
 	vec4 mvPosition = modelViewMatrix * instanceMatrix * vec4( 0.0, 0.0, 0.0, 1.0 );
-    mvPosition.xy += position.xy * uPointSize * size;
+
+    vec2 displacedPos = vec2(position.x + .4 * cos(uTime + displacement.x * 10.) * (color - .3), position.y + .4 * sin(uTime + displacement.y * 10.) * (color - .3));
+
+    mvPosition.xy += displacedPos.xy * uPointSize * size;
 	gl_Position = projectionMatrix * mvPosition;
 
     // Varyings
