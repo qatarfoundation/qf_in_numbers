8071a0189ad44d15fcd60a81c943c6dbffe9251f
Mathijs <mathijspb@gmail.com>
2022-08-29 11:30:38 +0200

    :globe_with_meridians: Update translations

--- src/components/ButtonDiscover/index.js
+++ src/components/ButtonDiscover/index.js
@@ -1,24 +1,26 @@
 // React
 import React from 'react';
+import { useTranslation, Link } from 'gatsby-plugin-react-i18next';
 
 // CSS
 import './style.scoped.scss';
 
 // Components
 import Arrow from '@/assets/icons/arrow.svg';
-import { Link } from 'gatsby-plugin-react-i18next';
 
 function ButtonPagination(props) {
     /**
      * Datas
      */
     const { name, slug } = props;
+    const { t } = useTranslation();
+
     return (
         <Link to={ slug ? slug : '' } { ...props } className={ `button button-discover ${ props.direction }` }>
             <div className="icon icon-arrow">
                 <Arrow className={ `arrow ${ props.direction }` } />
             </div>
-            <p>Tap to discover{ name && ` <br />${name}` }</p>
+            <p>{ `${ t('Tap to explore') }${ name ? ` <br />${ name }` : '' }` }</p>
         </Link>
     );
 }
