42e071658106098b2948ceee4c203bb3c8352616
Mathijs <mathijspb@gmail.com>
2022-09-14 15:10:03 +0200

    :lipstick: Subcategory > Button discover > Improve icon and hover animation

---
+++ src/components/ButtonDiscoverMobile/index.js
@@ -0,0 +1,28 @@
+// React
+import React from 'react';
+import { useTranslation, Link } from 'gatsby-plugin-react-i18next';
+
+// CSS
+import './style.scoped.scss';
+
+// Components
+import Arrow from '@/assets/icons/arrow.svg';
+
+function ButtonDiscoverMobile(props) {
+    /**
+     * Datas
+     */
+    const { name, slug } = props;
+    const { t } = useTranslation();
+
+    return (
+        <Link to={ slug ? slug : '' } { ...props } className={ `button button-discover ${ props.direction }` }>
+            <div className="icon icon-arrow">
+                <Arrow className={ `arrow ${ props.direction }` } />
+            </div>
+            <p>{ `${ t('Tap to explore') }${ name ? ` <br />${ name }` : '' }` }</p>
+        </Link>
+    );
+}
+
+export default ButtonDiscoverMobile;
