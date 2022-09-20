905b0d63a67ab5c21c42db9cd1a345a70b8e0584
David Bismut <david.bismut@gmail.com>
2022-09-20 22:54:07 +0200

    🌈 add a wrapper for fade in/out animations

--- src/components/FadeInWrapper/index.js
+++ src/components/FadeInWrapper/index.js
@@ -4,9 +4,16 @@ import { useInView } from 'react-intersection-observer';
 // CSS
 import './style.scoped.scss';
 
-function FadeInWrapper({ children }) {
-    const { ref, inView } = useInView();
-    return <div className={ 'fadein-container ' + inView ? 'visible' : '' } ref={ ref }>{ children }</div>;
+function FadeInWrapper({ children, threshold = 0.5 }) {
+    const { ref, inView } = useInView({ threshold });
+    return (
+        <div
+            className={ 'fadein-container' + (inView ? ' visible' : '') }
+            ref={ ref }
+        >
+            { children }
+        </div>
+    );
 }
 
 export default FadeInWrapper;
