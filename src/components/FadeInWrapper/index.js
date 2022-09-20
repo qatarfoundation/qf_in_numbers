14756c996b1901b1eae3eee380a61306d270d755
David Bismut <david.bismut@gmail.com>
2022-09-20 23:25:56 +0200

    fix: optimize fadeIn

--- src/components/FadeInWrapper/index.js
+++ src/components/FadeInWrapper/index.js
@@ -14,7 +14,9 @@ function FadeInWrapper({
     return (
         <InView
             as={ as }
+            initialInView={ false }
             threshold={ threshold }
+            rootMargin="0px 0px -80px 0px"
             className={ className + ' fadein-container' }
             onChange={ (inView, entry) =>
                 entry.target.classList.toggle('visible', inView)
